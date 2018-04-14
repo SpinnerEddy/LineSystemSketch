290aec659bce3e04f60ae324227fad344a5e41fd
Unique22 <tales.wrm.rl2102@icloud.com>
2018-04-14 22:43:54 +0900

    Particle class copy from ParticleSystemSketch

--- LineSketchSketch/src/Particle.cpp
+++ LineSketchSketch/src/Particle.cpp
@@ -6,3 +6,84 @@
 //
 
 #include "Particle.hpp"
+
+Particle::Particle(){
+    
+}
+
+Particle::Particle(float x,float y){
+    this->position.set(x,y);
+    this->velocity.set(ofRandom(-2,2),ofRandom(-2,2));
+    this->acceleration.set(0,0);
+    this->c.setHsb(ofRandom(255), 255, 255);
+    this->angle = 0;
+    this->aVelocity = 0.2f;
+    this->range = 2;
+    this->radius = ofRandom(10,40);
+    this->mass = 1;
+    this->lifeSpan = 255;
+    this->aVelocity = this->velocity.x / 20;
+}
+
+Particle::Particle(float x,float y,float h){
+    this->position.set(x,y);
+    this->velocity.set(ofRandom(-2,2),ofRandom(-2,2));
+    this->acceleration.set(0,0);
+    this->c.setHsb(h, 255, 255);
+    this->angle = 0;
+    this->radius = ofRandom(10,40);
+    this->mass = 1;
+    this->lifeSpan = 255;
+    this->aVelocity = this->velocity.x / 20;
+}
+
+void Particle::update(){
+    this->velocity += this->acceleration;
+    this->position += this->velocity;
+    this->acceleration *= 0;
+    this->lifeSpan -= 2.5;
+    this->angle += this->aVelocity;
+}
+
+void Particle::updateCircular(){
+    this->position.x = range * cos(angle);
+    this->position.y = range * sin(angle);
+}
+
+void Particle::applyForce(ofVec2f force){
+    ofVec2f f = force / this->mass;
+    this->acceleration += f;
+}
+
+void Particle::display(){
+    
+}
+
+void Particle::connect(vector<Particle*> ps){
+    for(int i = ps.size()-1; i >= 0; i--){
+        if(typeid(this) == typeid(ps[i]) && this != ps[i]){
+            float d = ofDist(this->position.x, this->position.y,
+                             ps[i]->getPosition().x, ps[i]->getPosition().y);
+            if(d < 80){
+                ofSetColor(this->c,this->lifeSpan);
+                ofDrawLine(this->position.x, this->position.y,
+                           ps[i]->getPosition().x, ps[i]->getPosition().y);
+            }
+        }
+    }
+}
+
+bool Particle::isDead(){
+    if(lifeSpan <= 0){
+        return true;
+    }
+    return false;
+}
+
+ofVec2f Particle::getPosition(){
+    return position;
+}
+
+ofColor Particle::getColor(){
+    return c;
+}
--- LineSketchSketch/src/Particle.hpp
+++ LineSketchSketch/src/Particle.hpp
@@ -9,5 +9,31 @@
 #define Particle_hpp
 
 #include <stdio.h>
+#include "ofMain.h"
 
+class Particle{
+protected:
+    ofVec2f position;
+    ofVec2f velocity;
+    ofVec2f acceleration;
+    ofColor c;
+    float radius;
+    float angle;
+    float mass;
+    float lifeSpan;
+    float range;
+    float aVelocity;
+public:
+    Particle();
+    Particle(float x,float y);
+    Particle(float x,float y,float h);
+    void update();
+    void updateCircular();
+    virtual void display();
+    void connect(vector<Particle*> ps);
+    bool isDead();
+    void applyForce(ofVec2f force);
+    ofVec2f getPosition();
+    ofColor getColor();
+};
 #endif /* Particle_hpp */
