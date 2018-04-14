167beb55dbdd01d44b8663437ae5b0cf6c8b1f4b
Unique22 <tales.wrm.rl2102@icloud.com>
2018-04-14 23:24:00 +0900

    Create Line Class

---
+++ LineSketchSketch/src/Line.cpp
@@ -0,0 +1,8 @@
+//
+//  Line.cpp
+//  LineSketchSketch
+//
+//  Created by 山下希流 on 2018/04/14.
+//
+
+#include "Line.hpp"
---
+++ LineSketchSketch/src/Line.hpp
@@ -0,0 +1,13 @@
+//
+//  Line.hpp
+//  LineSketchSketch
+//
+//  Created by 山下希流 on 2018/04/14.
+//
+
+#ifndef Line_hpp
+#define Line_hpp
+
+#include <stdio.h>
+
+#endif /* Line_hpp */
