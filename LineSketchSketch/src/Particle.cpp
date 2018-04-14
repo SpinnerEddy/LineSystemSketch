27b7b95d697a3721811fcfece906565b0fca6bd8
Unique22 <tales.wrm.rl2102@icloud.com>
2018-04-14 17:51:59 +0900

    Create New Class

---
+++ LineSketchSketch/src/Particle.cpp
@@ -0,0 +1,8 @@
+//
+//  Particle.cpp
+//  LineSketchSketch
+//
+//  Created by 山下希流 on 2018/04/14.
+//
+
+#include "Particle.hpp"
---
+++ LineSketchSketch/src/Particle.hpp
@@ -0,0 +1,13 @@
+//
+//  Particle.hpp
+//  LineSketchSketch
+//
+//  Created by 山下希流 on 2018/04/14.
+//
+
+#ifndef Particle_hpp
+#define Particle_hpp
+
+#include <stdio.h>
+
+#endif /* Particle_hpp */
