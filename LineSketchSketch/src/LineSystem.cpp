df2d854ac07afdefef7aae57d1251620ea7561da
Unique22 <tales.wrm.rl2102@icloud.com>
2018-04-15 00:35:21 +0900

    create LineSystem class file

---
+++ LineSketchSketch/src/LineSystem.cpp
@@ -0,0 +1,8 @@
+//
+//  LineSystem.cpp
+//  LineSketchSketch
+//
+//  Created by 山下希流 on 2018/04/15.
+//
+
+#include "LineSystem.hpp"
---
+++ LineSketchSketch/src/LineSystem.hpp
@@ -0,0 +1,13 @@
+//
+//  LineSystem.hpp
+//  LineSketchSketch
+//
+//  Created by 山下希流 on 2018/04/15.
+//
+
+#ifndef LineSystem_hpp
+#define LineSystem_hpp
+
+#include <stdio.h>
+
+#endif /* LineSystem_hpp */
